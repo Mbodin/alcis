4eb52209221c6f8cfb3ec3b633b15dd01926ba5a
Martin Bodin <martin.bodin@ens-lyon.org>
2010-08-18 22:27:58 +0200

    I reorganize choices.ml and it permit the help to be clearer.

--- src/interface/choices.ml
+++ src/interface/choices.ml
@@ -15,18 +15,25 @@ type arg =
 let options = Hashtbl.create 100
 let actions = Hashtbl.create 100
 
+let max_size_action_name = ref 0
+
+let add_action name nb_arg descr f =
+    Hashtbl.add actions name (f, nb_arg, descr);
+    max_size_action_name := max !max_size_action_name (String.length name);
+    ()
+
 let add_boolean_option name default description =
     Hashtbl.add options name (Bool default);
-    Hashtbl.add actions ("-" ^ name)
-    ((function
+    add_action ("-" ^ name) 0 description
+    (function
         | [] -> Hashtbl.add options name (Bool true)
         | l -> internal_error ["The option “-" ^ name ^ "” requests no argument, but it is called with " ^ (string_of_int (List.length l)) ^ " ones."]
-    ), 0, description);
-    Hashtbl.add actions ("-no-" ^ name)
-    ((function
+    );
+    add_action ("-no-" ^ name) 0 description
+    (function
         | [] -> Hashtbl.add options name (Bool false)
         | l -> internal_error ["The option “-no-" ^ name ^ "” requests no argument, but it is called with " ^ (string_of_int (List.length l)) ^ " ones."]
-    ), 0, "Disable the option -" ^ name);
+    );
     ()
 
 let get_value name =
@@ -47,7 +54,7 @@ let do_action action args =
         try Hashtbl.find actions action with
         | Not_found ->
                 internal_error ["The action associated to the option “" ^ action ^ "” is requested to be called, but no such option exists.";
-                "For information, there was too " ^ (string_of_int (List.length args)) ^ " argument given to it."]
+                "For information, there was " ^ (string_of_int (List.length args)) ^ " arguments given to it."]
     in
     if n <> List.length args then
         internal_error ["The action associated to the option “" ^ action ^ "” received " ^ (string_of_int (List.length args)) ^ " arguments, but " ^ (string_of_int n) ^ " where expected."]
@@ -55,15 +62,13 @@ let do_action action args =
 
 let list_options () =
     Hashtbl.iter 
-    (fun name -> fun (_, _, desc) -> print_string ("\t" ^ name ^ "\t\t" ^ desc ^ "\n"))
+    (fun name -> fun (_, _, desc) -> print_string ("\t" ^ name ^ (String.make (!max_size_action_name - String.length name) ' ') ^ "\t\t" ^ desc ^ "\n"))
     actions
 
-let help_action =
-    ((function
+let help_action = function
         | [] -> list_options ()
         | _ -> internal_error ["There was at least an argument given to the help action."; "I’m very sorry you to read this: you probably obtain it while calling help."]
-    ), 0, "Display this help")
 
-let _ = Hashtbl.add actions "-help" help_action
-let _ = Hashtbl.add actions "-h" help_action
+let _ = add_action "-help" 0 "Display this help" help_action
+let _ = add_action "-h" 0 "Display this help" help_action
 
