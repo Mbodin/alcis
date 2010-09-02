83dd155c1ce56755d89850a21393c14004597f36
Martin Bodin <martin.bodin@ens-lyon.org>
2010-09-02 18:38:46 +0200

    Now it’s supposed to work...

--- src/parsing/parser_printer.ml
+++ src/parsing/parser_printer.ml
@@ -5,10 +5,14 @@
 let _ = Choices.add_action "-o-lexemes" 1 ["file"] "(Only used by developpers)"
     (function
         | f :: [] ->
-                let out = open_out f in
-                let channel = open_in "FIXME" in
-                let buf = Lexing.from_channel channel in
-                List.iter (fun s -> output_string out (s ^ " ")) (Preparser.lex_flot Lexer.token buf)
+            let out = if f = "-" then stdout else open_out f in
+            List.iter
+            (fun (channel, _) ->
+                    let buf = Lexing.from_channel channel in
+                    List.iter (fun s -> output_string out (s ^ " ")) (Preparser.lex_flot Lexer.token buf))
+            (match Choices.get_value "input" with
+            | Choices.Input_list l -> l
+            | _ -> Errors.internal_error ["The option “input” does not contain an input list."]) (* FIXME: A generic message for this. *)
         | l -> Choices.wrong_arg_number_error "-o-lexemes" 1 l
     )
 
