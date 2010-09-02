755e97ad4a5762a0a111b5a04f71393cd675e52c
Martin Bodin <martin.bodin@ens-lyon.org>
2010-09-02 19:05:33 +0200

    It sould work but it seems not with the standard input.

--- src/parsing/parser_printer.ml
+++ src/parsing/parser_printer.ml
@@ -8,8 +8,13 @@ let _ = Choices.add_action "-o-lexemes" 1 ["file"] "(Only used by developpers)"
             let out = if f = "-" then stdout else open_out f in
             List.iter
             (fun (channel, _) ->
+                    Lexer.eof_reached := false;
                     let buf = Lexing.from_channel channel in
-                    List.iter (fun s -> output_string out (s ^ " ")) (Preparser.lex_flot Lexer.token buf))
+                    while !Lexer.eof_reached = false
+                    do
+                        let result = Preparser.lex_flot Lexer.token buf in
+                        List.iter (fun s -> output_string out (s ^ " ")) result
+                    done)
             (match Choices.get_value "input" with
             | Choices.Input_list l -> l
             | _ -> Errors.internal_error ["The option “input” does not contain an input list."]) (* FIXME: A generic message for this. *)
